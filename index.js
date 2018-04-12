df5cee34545e23392841ee42c6271c59409ebd1a
durward <durward3@cox.net>
2018-04-12 10:16:23 -0400

    attempt at dynamic urls pt 1

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/', function(request, response) {
 
 app.get('/senator/:id', function(request, response) {
   var id = request.params.id;
-  var joinquery = `SELECT * FROM senators s, bills b, billsponsors bs WHERE s.senid = bs.senid AND b.billid = bs.billid AND senid = '${}';`;
+  var joinquery = `SELECT * FROM senators s, bills b, billsponsors bs WHERE s.senid = bs.senid AND b.billid = bs.billid AND senid = '${id}';`;
 
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query(senatorsquery, function(err, result) {
