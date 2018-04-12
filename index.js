db19cb8421002aaa33261230ee43bf16389d0e51
durward <durward3@cox.net>
2018-04-12 02:01:53 -0400

    initial web view

--- index.js
+++ index.js
@@ -20,5 +20,21 @@ app.set('view engine', 'ejs');
 // index node
 app.get('/', function(request, response) {
     console.log("Main page");
-    response.render('pages/main-page');
+
+    var senatorsquery = 'SELECT * FROM senators;'
+
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query(senatorsquery, function(err, result) {
+        done();
+        if (err) {
+          console.error(err);
+          response.sent("Error " + err);
+        } else {
+          response.render('pages/main-page', {senators: result.rows});
+        }
+      })
+    });
+});
+
+app.listen(app.get('port'), function() {
 });
