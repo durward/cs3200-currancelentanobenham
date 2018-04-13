b3cf63b2a0481bc6d4c06faefafb632399e1e08d
durward <durward3@cox.net>
2018-04-13 19:50:15 -0400

    cleaning up some old code

--- index.js
+++ index.js
@@ -14,12 +14,10 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
-
-app.use()
+app.use(bodyParser.json());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
