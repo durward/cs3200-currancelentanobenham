db3ee17eef9e7cbde31d2f741820b439299ccc47
durward <durward3@cox.net>
2018-04-19 19:10:20 -0400

    refactoring update to handle errors

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.post('/senator/:id', function(request, response) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
       reloadsenatorpage(client, response, joinquery,
         "Invalid update. Make sure no field was left blank (except for optional  website) and party is one of [Independent, Republican, Democratic]");
-    }
+    });
   } else {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
       client.query(newquery, function(err, result) {
